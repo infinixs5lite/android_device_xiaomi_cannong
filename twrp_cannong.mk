040afa67e0e9bbb8da690b7808dfb4c3bd19baaa
infinixs5lite <kelvinchinedu97@gmail.com>
2022-08-25 09:23:25 +0100

    Update twrp_cannong.mk

--- twrp_cannong.mk
+++ twrp_cannong.mk
@@ -6,9 +6,8 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Inherit from cannong device
 $(call inherit-product, device/xiaomi/cannong/device.mk)
