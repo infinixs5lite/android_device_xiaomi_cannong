966604afefcdd415c07465760acdb955024ec270
infinixs5lite <kelvinchinedu97@gmail.com>
2022-08-25 07:31:24 +0100

    Update omni_cannong.mk

--- omni_cannong.mk
+++ omni_cannong.mk
@@ -13,13 +13,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # Inherit from cannong device
 $(call inherit-product, device/xiaomi/cannong/device.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/twrp/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := cannong
-PRODUCT_NAME := omni_cannong
+PRODUCT_NAME := twrp_cannong
 PRODUCT_BRAND := Redmi
-PRODUCT_MODEL := M2007J22G
+PRODUCT_MODEL := Redmi note 9T
 PRODUCT_MANUFACTURER := xiaomi
